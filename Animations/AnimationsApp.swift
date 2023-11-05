ef0d9cfdad41d38c471bd4d82bf4aa4b89bf77b8
Victor Barrios <victor.barrios@bestsecret.com>
2023-11-06 00:32:12 +0100

    Initial Commit

---
+++ Animations/AnimationsApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimationsApp.swift
+//  Animations
+//
+//  Created by Barrios, Victor on 6/11/23.
+//
+
+import SwiftUI
+
+@main
+struct AnimationsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
