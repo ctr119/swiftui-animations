11c4a875edf9abe9a259e1a021264c8609ee96db
Victor Barrios <victor.barrios@bestsecret.com>
2023-11-07 12:14:02 +0100

    Separate animations in different comp properties

--- Animations/ContentView.swift
+++ Animations/ContentView.swift
@@ -1,21 +1,47 @@
-//
-//  ContentView.swift
-//  Animations
-//
-//  Created by Barrios, Victor on 6/11/23.
-//
-
 import SwiftUI
 
 struct ContentView: View {
+    @State private var bounceAmount = 1.0
+    @State private var sonarAmount = 1.0
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        VStack(spacing: 100) {
+            bouncingButton
+            
+            sonarButton
         }
-        .padding()
+    }
+    
+    private var bouncingButton: some View {
+        mainButton
+            .scaleEffect(bounceAmount)
+            .animation(.spring(response: 0.33, dampingFraction: 0.33, blendDuration: 0.5),
+                       value: bounceAmount)
+    }
+    
+    private var sonarButton: some View {
+        mainButton
+            .overlay {
+                Circle()
+                    .stroke(.red)
+                    .scaleEffect(sonarAmount)
+                    .opacity(2 - sonarAmount)
+                    .animation(.easeInOut(duration: 1).repeatForever(autoreverses: false),
+                               value: sonarAmount)
+            }
+            .onAppear {
+                sonarAmount = 2
+            }
+    }
+    
+    private var mainButton: some View {
+        Button("Tap Me") {
+            bounceAmount += 1
+        }
+        .padding(50)
+        .background(.red)
+        .foregroundStyle(.white)
+        .clipShape(Circle())
     }
 }
 
